46c5fc6eb20fc0dae275083678c0d51b033d186c
Sebastian Green-Husted <ricecake@tfm.nu>
2015-09-13 16:09:09 -0400

    Update authenticator.sql

--- setupAssets/authenticator.sql
+++ setupAssets/authenticator.sql
@@ -1,3 +1,20 @@
 BEGIN;
 
+CREATE TABLE domain
+Create table user
+Create table action
+Create table role
+Create table group
+Create table ratelimit
+Create table user_domain
+Create table user_action
+Create table user_role
+Create table user_group
+Create table user_ratelimit
+Create table group_role
+Create table group_action
+Create table group_ratelimit
+Create table role_action
+Create table role_ratelimit
+
 COMMIT;
